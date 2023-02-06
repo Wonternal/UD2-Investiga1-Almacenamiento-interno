d9e69306aa0cf4dde6faf3a4380565d14f14396a
Eduardo Camacho Díaz <fgada200@gmail.com>
2023-02-06 13:02:10 +0000

    Acabado

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const storeData = async (text) => {
+        try {
+            await AsyncStorage.setItem("@contrasena", text);
+            console.log("guardando contraseña -> ", text);
+        } catch (error) {
+            // Error saving data
+        }
+    };
+
+    const getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem("@contrasena");
+
+            if (value !== null) {
+                setStoredText(value);
+            }
+            return null;
+        } catch (e) {
+            // error reading value
+        }
+    };
+
+    const [textInput, setTextInput] = useState("");
+    const [storedText, setStoredText] = useState("");
+    return (
+        <View style={styles.container}>
+            <TextInput onChangeText={(text) => setTextInput(text)} placeholder="Texto a guardar en memoria"></TextInput>
+            <Button onPress={() => storeData(textInput)} title="Guardar"></Button>
+            <StatusBar style="auto" />
+            <Button onPress={() => getData()} title="Leer datos almacenados"></Button>
+            <Text>{storedText}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
